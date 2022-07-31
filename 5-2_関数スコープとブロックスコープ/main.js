14720f570b391889d82b4c91fa6718545ea016d1
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-07-31 23:46:39 +0900

    [finish]5-2

---
+++ 5-2_関数スコープとブロックスコープ/main.js
@@ -0,0 +1,24 @@
+function a() {
+  let b = 0;
+  console.log(b);
+}
+// console.log(b);
+a();
+
+if(true){
+  // let c = 1;
+  // var c = 1;
+  const c = 1;
+  console.log(c);
+
+  function d() {
+    console.log('d is called');
+  }
+
+  // const d = function() {
+  //   console.log('d is called');
+  // }
+}
+
+d();
+// console.log(c);
