007d2ebaca355e756d45c0eceeaafb4c4c7c29b1
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-02 09:53:58 +0900

    [finish]5-6

---
+++ 5-6_クロージャー（動的関数）/main.js
@@ -0,0 +1,13 @@
+function addNumberFactory(num) {
+  function addNumber(value) {
+    console.log(value);
+    return num + value;
+  }
+  return addNumber;
+}
+
+const add5 = addNumberFactory(5);
+const add10 = addNumberFactory(10);
+const result = add10(10);
+console.log(result);
+
