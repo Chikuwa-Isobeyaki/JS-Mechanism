ebc0d08f60630462ba187dd703c0833feb66dad0
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-03 16:29:29 +0900

    [Finish]5-10

--- 5-10_修了確認_即時関数/start/main.js
+++ 5-10_修了確認_即時関数/start/main.js
@@ -27,7 +27,32 @@ function calcFactory(val) {
         },
     }
 }
-const calc = calcFactory(10);
+
+const calc = (function(val) {
+    return {
+        plus: function(target) {
+            const newVal = val + target;
+            console.log(`${val} + ${target} = ${newVal}`);
+            val = newVal;
+        },
+        minus: function(target) {
+            const newVal = val - target;
+            console.log(`${val} - ${target} = ${newVal}`);
+            val = newVal;
+        },
+        multiply: function(target) {
+            const newVal = val * target;
+            console.log(`${val} x ${target} = ${newVal}`);
+            val = newVal;
+        },
+        divide: function(target) {
+            const newVal = val / target;
+            console.log(`${val} / ${target} = ${newVal}`);
+            val = newVal;
+        },
+    }
+})(10)
+
 calc.plus(5);
 calc.minus(3);
 calc.multiply(3);
