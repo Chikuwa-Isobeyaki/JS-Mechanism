d065fac1cbbf104d245469ce5aa7c1149021baac
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-05 11:09:24 +0900

    [finish]7-5

---
+++ 7-5_参照のコピーとthis/main.js
@@ -0,0 +1,24 @@
+window.name = 'John';
+
+const person = {
+  name: 'Tom',
+  hello: function() {
+    console.log('Hello ' + this.name);
+    a();
+    const person = {
+      name: 'Tim',
+      hello: function() {
+        console.log('Hello ' + this.name);
+        a();
+      }
+    }
+    person.hello();
+  }
+}
+
+person.hello();
+//ref();
+
+function a() {
+  console.log('Hello ' + this.name);
+}
