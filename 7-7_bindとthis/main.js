1b02c22b233c007936b2e6bd28eb89fd67de788c
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-05 13:58:07 +0900

    [Finish]7-8

---
+++ 7-7_bindとthis/main.js
@@ -0,0 +1,27 @@
+window.name = 'John';
+
+const person = {
+  name: 'Tom',
+  hello: function() {
+    console.log('Hello ' + this.name);
+  }
+}
+
+person.hello();
+
+const helloTom = person.hello.bind(person);
+
+function fn(ref) {
+  ref();
+}
+
+fn(helloTom);
+
+
+function a() {
+  console.log('hello ' + this.name);
+}
+
+const b = a.bind({name: 'Tim'});
+
+b();
