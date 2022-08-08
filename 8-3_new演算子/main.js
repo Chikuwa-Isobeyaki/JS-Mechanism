c27f6fe488a3c3dee58d73bd13ff0b3cef32ff8b
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-08 09:57:48 +0900

    [Finish]8-5

--- 8-3_new演算子/main.js
+++ 8-3_new演算子/main.js
@@ -1,7 +1,7 @@
 function F(a, b) {
   this.a = a;
   this.b = b;
-  return {};
+  return {a: 1};
 }
 
 F.prototype.c = function() {}
@@ -9,12 +9,13 @@ F.prototype.c = function() {}
 function newOpe(C, ...args) {
   const _this = Object.create(C.prototype);
   const result = C.apply(_this, args);
-  console.log(result, _this);
   console.log(typeof result);
   if(typeof result === "object" && result !== null) {
-    return result;
+      return result;
   }
+
+  return _this;
 }
 
 const instance = newOpe(F, 1, 2);
-// console.log(instance);
+console.log(instance)
